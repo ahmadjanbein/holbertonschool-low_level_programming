373cb8f5cb1e9096b7f967e8a48183cb72fc5457
ahmadjanbein <janbeinahmad5@gmail.com>
2023-10-15 11:07:11 -0700

    jojoj

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description:A C program that prints with put function
  *
  * Return: Always 0 (Success)
 */
@@ -11,6 +11,6 @@
 int main(void)
 
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
