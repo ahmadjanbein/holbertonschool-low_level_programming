550dbf8bc8da702bd3fd1ee13fa6ff2594a88f99
ahmadjanbein <janbeinahmad5@gmail.com>
2023-10-15 13:20:04 -0700

    jojj

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints the size of various computer types
+ * main - Entry point
+ * Description: using sizeof to print the  size of varius  types
  * Return 0 (Success)
 */
 int main(void)
